3dc69923209d09f68f63872805c8f89cfa46502c
Raunak aggarwal <raunakaggarwal1408@gmail.com>
2026-09-19 22:05:46 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const buttons = document.querySelectorAll('.add-to-cart');
-    buttons.forEach(button => {
-        button.addEventListener('click', function() {
-            alert('Item added to cart!');
-        });
-    });
-});
